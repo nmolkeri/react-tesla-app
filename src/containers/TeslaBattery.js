c1b9022395426ab89747cd503f15215abadfcc8e
Niranjan Molkeri <nmolkeri@gmail.com>
2017-08-17 16:45:50 -0700

    header component

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './TeslaBattery.css';
+
+class TeslaBattery extends Component {
+  render() {
+    return(
+      <form className="tesla-battery">
+        <h1>Range Per Charge</h1>
+      </form>
+    );
+  }
+}
