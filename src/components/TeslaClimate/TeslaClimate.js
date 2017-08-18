9e420893fb431018df3501c74e3eaa0df20b2637
Niranjan Molkeri <nmolkeri@gmail.com>
2017-08-17 18:48:59 -0700

    tesla climate control

---
+++ src/components/TeslaClimate/TeslaClimate.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TeslaClimate.css';
+import PropTypes from 'prop-types';
+
+const TeslaClimate = (props) => (
+  <div className="tesla-climate">
+    <label
+      className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}`}
+    >
+      <p>{props.limit ? 'ac' : 'heat'} {props.value ? 'on' : 'off'}</p>
+      <i className="tesla-climate__icon"></i>
+      <input
+        type="checkbox"
+        name="climate"
+        checked={props.value}
+        onChange={() => {props.handleChangeClimate()}}
+      />
+    </label>
+  </div>
+);
+TeslaClimate.propTypes = {
+  value: React.PropTypes.bool,
+  limit: React.PropTypes.bool,
+  handleChangeClimate: React.PropTypes.func
+}
+export default TeslaClimate;
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -3,6 +3,8 @@ import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
 import TeslaStats from '../components/TeslaStats/TeslaStats';
+import TeslaCounter from '../components/TeslaCounter/TeslaCounter';
+import TeslaClimate from '../components/TeslaClimate/TeslaClimate';
 import { getModelData } from '../services/BatteryService';
 
 class TeslaBattery extends Component {
@@ -20,20 +22,76 @@ class TeslaBattery extends Component {
 
     this.calculateStats = this.calculateStats.bind(this);
     this.statsUpdate = this.statsUpdate.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.updateCounterState = this.updateCounterState.bind(this);
+    this.handleChangeClimate = this.handleChangeClimate.bind(this);
+
   }
 
-    calculateStats = (models, value) => {
-    const dataModels = getModelData();
-    return models.map(model => {
-      // ES6 Object destructuring Syntax,
-      // takes out required values and create references to them
-      const { speed, temperature, climate, wheels } = value;
-      const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
-      return {
-        model,
-        miles
-      };
-    });
+  updateCounterState(title, newValue) {
+    const config = { ...this.state.config };
+    // update config state with new value
+    title === 'Speed' ? config['speed'] = newValue : config['temperature'] = newValue;
+    // update our state
+    this.setState({ config });
+  }
+  calculateStats = (models, value) => {
+  const dataModels = getModelData();
+  return models.map(model => {
+    // ES6 Object destructuring Syntax,
+    // takes out required values and create references to them
+    const { speed, temperature, climate, wheels } = value;
+    const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+    return {
+      model,
+      miles
+    };
+  });
+  }
+
+  increment(e, title) {
+    e.preventDefault();
+    let currentValue, maxValue, step;
+    const { speed, temperature } = this.props.counterDefaultVal;
+    if (title === 'Speed') {
+      currentValue = this.state.config.speed;
+      maxValue = speed.max;
+      step = speed.step;
+    } else {
+      currentValue = this.state.config.temperature;
+      maxValue = temperature.max;
+      step = temperature.step;
+    }
+    if (currentValue < maxValue) {
+      const newValue = currentValue + step;
+      this.updateCounterState(title, newValue);
+    }
+  }
+
+  decrement(e, title) {
+    e.preventDefault();
+    let currentValue, minValue, step;
+    const { speed, temperature } = this.props.counterDefaultVal;
+    if (title === 'Speed') {
+      currentValue = this.state.config.speed;
+      minValue = speed.min;
+      step = speed.step;
+    } else {
+      currentValue = this.state.config.temperature;
+      minValue = temperature.min;
+      step = temperature.step;
+    }
+    if (currentValue > minValue) {
+      const newValue = currentValue - step;
+      this.updateCounterState(title, newValue);
+    }
+  }
+
+  handleChangeClimate() {
+  const config = {...this.state.config};
+  config['climate'] = !this.state.config.climate;
+  this.setState({ config });
   }
 
   statsUpdate() {
@@ -55,6 +113,29 @@ class TeslaBattery extends Component {
         <h1>Range Per Charge</h1>
         <TeslaCar wheelsize={config.wheels}/>
         <TeslaStats carstats={carstats}/>
+        <div className="tesla-controls cf">
+          <TeslaCounter
+            currentValue={this.state.config.speed}
+            initValues={this.props.counterDefaultVal.speed}
+            increment={this.increment}
+            decrement={this.decrement}
+          />
+          <div className="tesla-climate-container cf">
+            <TeslaCounter
+              currentValue={this.state.config.temperature}
+              initValues={this.props.counterDefaultVal.temperature}
+              increment={this.increment}
+              decrement={this.decrement}
+            />
+            <TeslaClimate
+            value={this.state.config.climate}
+            limit={this.state.config.temperature > 10}
+            handleChangeClimate={this.handleChangeClimate}
+            />
+          </div>
+        </div>
+
+
         <TeslaNotice />
       </form>
     );
