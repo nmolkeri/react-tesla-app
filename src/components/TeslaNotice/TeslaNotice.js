a78dab2342f1f312b816af12ac2c05f491191d83
Niranjan Molkeri <nmolkeri@gmail.com>
2017-08-17 18:26:56 -0700

    tesla stats

---
+++ src/components/TeslaNotice/TeslaNotice.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TeslaNotice.css';
+
+const TeslaNotice = () => (
+  <div className="tesla-battery__notice">
+    <p>
+      The actual amount of range that you experience
+        will vary based on your usage. See how your
+          usage affect your range in this simulation applciation.
+    </p>
+    <p>
+    Vehicle range may vary depending on the vehicle configuration,
+    battery age and condition, driving style and operating, environmental
+    and climate conditions.
+    </p>
+  </div>
+)
+
+export default TeslaNotice;
